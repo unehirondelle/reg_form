3685a8b9a519145c1d6fcd4f71c60db5fa26ecc3
Irina Plaksina <irene.plaksina@gmail.com>
2019-01-28 19:20:25 -0600

    test data change

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 const express = require('express'); //create a function and store it in the const;
-const app = express() //call the function and store the result in the const;
-const port = process.env.PORT || 3010;
+const app = express(); //call the function and store the result in the const;
+const port = process.env.PORT || 3000; //set the environment variable
 
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname + "/public/" } );
+}); //make the default page
+
+/*enable parsing of json objects in a body of a request to execute the POST method
+(it's disabled by default):*/
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //use the part of middleware in the app
 
-app.get('/', (req, res) => res.send('Hello World!')); //get the data (incoming request to the server) from root ('/'); req (request) and res (result) are arguments of a callback function;
-app.use('/static', express.static('public'));
 
-app.post('/results', (req, res) => {
-    let firstName = request.body.firstName;
-    let lastName = request.body.lastName;
+app.get('/', (req, res) => res.send('Hello World!!'));
+/*get the data (incoming request to the server) from root ('/');
+req (request) and res (result) are arguments of a callback function;*/
+
+app.use ('/static', express.static('public'));
+
+app.post('/results', function (request, response) {
+    var firstName = request.body.firstName;
+    var lastName = request.body.lastName;
     console.log(firstName, lastName);
     for (k in request.headers){
-        console.log(k, request.headers[k])
+        console.log(k, request.headers[k]);
     }
 });
 
 console.log('started @' + new Date())
-console.log(module)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
